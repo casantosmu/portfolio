75f1b4716a7e8ac903f686309fd9d54d3991562b
Carlos Santos <casantosmu@gmail.com>
2025-03-19 21:36:11 +0100

    Replace checkbox filters with accessible radio buttons for mutually exclusive tag selection

--- static/script.js
+++ static/script.js
@@ -1,44 +1,18 @@
 const projects = document.querySelectorAll(".js-project");
-
 const filter = document.querySelector("#projects-filter");
-const checkboxes = filter.querySelectorAll("input[type='checkbox']");
 const allCheckbox = filter.querySelector("input[value='all']");
-
-const selectedTags = new Set();
 
 filter.addEventListener("click", (event) => {
   const target = event.target;
-
-  if (target === allCheckbox) {
-    selectedTags.clear();
-
-    for (const project of projects) {
-      project.style.display = "";
-    }
-    for (const checkbox of checkboxes) {
-      checkbox.checked = false;
-    }
-
-    allCheckbox.checked = true;
+  if (target.type !== "radio") {
     return;
   }
 
-  if (target.type === "checkbox") {
-    const tag = target.value;
+  const isAll = target.value === "all";
 
-    if (target.checked) {
-      selectedTags.add(tag);
-    } else {
-      selectedTags.delete(tag);
-    }
-
-    const isAll = selectedTags.size === 0;
-    for (const project of projects) {
-      const projectTags = project.dataset.tags.slice(1, -1).split(" ");
-      const matches = projectTags.some((tag) => selectedTags.has(tag));
-      project.style.display = matches || isAll ? "" : "none";
-    }
-
-    allCheckbox.checked = isAll;
+  for (const project of projects) {
+    const projectTags = project.dataset.tags.slice(1, -1).split(" ");
+    const matches = projectTags.some((tag) => tag === target.value);
+    project.style.display = matches || isAll ? "" : "none";
   }
 });
